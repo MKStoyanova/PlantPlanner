e1adb1bdda6bbe417ffc15c3bd460b9461eae59c
MKStoyanova <stoyanova.marina03@gmail.com>
2026-02-15 22:04:13 +0200

    Add WateringLogController with history action

---
+++ Controllers/WateringLogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlantPlanner.Data;
+
+namespace PlantPlanner.Controllers
+{
+    public class WateringLogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WateringLogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int plantId)
+        {
+            var logs = await _context.WateringLogs
+                .Where(w => w.PlantId == plantId)
+                .OrderByDescending(w => w.WateredOn)
+                .ToListAsync();
+
+            ViewBag.PlantId = plantId;
+
+            return View(logs);
+        }
+    }
+}
