b3b81ad3f00d48c33c4ebd819b9d214f1238d8cf
MKStoyanova <stoyanova.marina03@gmail.com>
2026-04-06 12:32:11 +0300

    Add identity login partial and navigation integration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PlantPlanner.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace PlantPlanner.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using PlantPlanner.Data;
 using PlantPlanner.Services.Contracts;
 using PlantPlanner.Services.Core;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,15 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IPlantService, PlantService>();
 
@@ -28,10 +38,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); 
+app.UseAuthorization();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
